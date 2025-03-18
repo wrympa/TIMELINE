58e18b0e0849d1a027e9ec6e3a1982b5ad025ce5
wrympa <shanidzeshinka@gmail.com>
2025-03-18 21:05:58 +0400

    trying things, but on this hosting branch

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+import uvicorn
+
+from apiApplications.AddressAPI import AddressAPI
+
+app = AddressAPI().app
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = int(os.environ.get("PORT", 9090))
+    uvicorn.run(app, host=host, port=port)
